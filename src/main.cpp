204fa2a614b193e565038a930de0e01b6ee6615b
Travis Cunningham <traviscunn94@gmail.com>
2024-01-20 17:03:03 -0800

    Working e-ink display code

--- src/main.cpp
+++ src/main.cpp
@@ -1,113 +1,67 @@
-#include <TinyGPSPlus.h>
-#include <SoftwareSerial.h>
-// #include <gpio_viewer.h> // Must me the first include in your project
-// GPIOViewer gpio_viewer;
+// GxEPD2_HelloWorld.ino by Jean-Marc Zingg
+//
+// Display Library example for SPI e-paper panels from Dalian Good Display and boards from Waveshare.
+// Requires HW SPI and Adafruit_GFX. Caution: the e-paper panels require 3.3V supply AND data lines!
+//
+// Display Library based on Demo Example from Good Display: https://www.good-display.com/companyfile/32/
+//
+// Author: Jean-Marc Zingg
+//
+// Version: see library.properties
+//
+// Library: https://github.com/ZinggJM/GxEPD2
 
-static const int RXPin = 17, TXPin = 16;
-static const uint32_t GPSBaud = 9600;
+// Supporting Arduino Forum Topics (closed, read only):
+// Good Display ePaper for Arduino: https://forum.arduino.cc/t/good-display-epaper-for-arduino/419657
+// Waveshare e-paper displays with SPI: https://forum.arduino.cc/t/waveshare-e-paper-displays-with-spi/467865
+//
+// Add new topics in https://forum.arduino.cc/c/using-arduino/displays/23 for new questions and issues
 
-// The TinyGPSPlus object
-TinyGPSPlus gps;
+// see GxEPD2_wiring_examples.h for wiring suggestions and examples
+// if you use a different wiring, you need to adapt the constructor parameters!
 
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
+// uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
+// #include <GFX.h>
 
-void displayInfo()
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+GxEPD2_3C<GxEPD2_750c_Z08, GxEPD2_750c_Z08::HEIGHT> display(GxEPD2_750c_Z08(/*CS=5*/ D9, /*DC=*/SCL, /*RST=*/SDA, /*BUSY=*/D7));
+
+const char HelloWorld[] = "Hello World!";
+void helloWorld()
 {
-  Serial.print(F("Location: "));
-  if (gps.location.isValid())
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby;
+  uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
   {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10)
-    {
-      Serial.print(F("0"));
-    }
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10)
-    {
-      Serial.print(F("0"));
-    }
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10)
-    {
-      Serial.print(F("0"));
-    }
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10)
-      Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.println();
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  } while (display.nextPage());
 }
 
 void setup()
 {
-  Serial.begin(115200);
+  // Serial.begin(115200);
 
-  // gpio_viewer.connectToWifi("Crunk Castle", "llamas123");
-
-  ss.begin(GPSBaud);
-
-  Serial.println(F("DeviceExample.ino"));
-  Serial.println(F("A simple demonstration of TinyGPSPlus with an attached GPS module"));
-  Serial.print(F("Testing TinyGPSPlus library v. "));
-  Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println(F("by Mikal Hart"));
-  Serial.println();
-
-  // gpio_viewer.begin();
+  // Serial.println(F("setup()"));
+  //   display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  // Serial.println(F("display.init() done"));
+  helloWorld();
+  // Serial.println(F("helloWorld() done"));
+  display.hibernate();
+  // Serial.println(F("display.hibernate() done"));
 }
 
-void loop()
-{
-  // This sketch displays information every time a new sentence is correctly encoded.
-  while (ss.available() > 0)
-  {
-    gps.encode(ss.read());
-  }
-
-  if (gps.time.isUpdated())
-  {
-    displayInfo();
-  }
-
-  if (millis() > 5000 && gps.charsProcessed() < 10)
-  {
-    Serial.println(F("No GPS detected: check wiring."));
-    while (true)
-      ;
-  }
-}
+void loop(){};
