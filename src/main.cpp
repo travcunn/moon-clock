142dc71ff7c0ff5624628888149d015736facd6e
Travis Cunningham <traviscunn94@gmail.com>
2024-01-20 17:50:39 -0800

    Update display based on gps coords

--- src/main.cpp
+++ src/main.cpp
@@ -25,18 +25,29 @@
 
 #include <GxEPD2_3C.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
 
+static const int RXPin = 17, TXPin = 16;
+static const uint32_t GPSBaud = 9600;
+
+// The TinyGPSPlus object
+TinyGPSPlus gps;
+
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
+
+// Wave e-ink display
 GxEPD2_3C<GxEPD2_750c_Z08, GxEPD2_750c_Z08::HEIGHT> display(GxEPD2_750c_Z08(/*CS=5*/ D9, /*DC=*/SCL, /*RST=*/SDA, /*BUSY=*/D7));
 
-const char HelloWorld[] = "Hello World!";
-void helloWorld()
+void printDisplayMessage(String time)
 {
   display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
   int16_t tbx, tby;
   uint16_t tbw, tbh;
-  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  display.getTextBounds(time, 0, 0, &tbx, &tby, &tbw, &tbh);
   // center the bounding box by transposition of the origin:
   uint16_t x = ((display.width() - tbw) / 2) - tbx;
   uint16_t y = ((display.height() - tbh) / 2) - tby;
@@ -46,22 +57,102 @@ void helloWorld()
   {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
-    display.print(HelloWorld);
+    display.print(time);
   } while (display.nextPage());
+}
+
+String buffer;
+
+void displayInfo()
+{
+
+  if (gps.location.isValid())
+  {
+    buffer = "Location: " + String(gps.location.lat()) + " " + String(gps.location.lng());
+    Serial.println(buffer);
+    printDisplayMessage(buffer);
+    display.hibernate();
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10)
+    {
+      Serial.print(F("0"));
+    }
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10)
+    {
+      Serial.print(F("0"));
+    }
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10)
+    {
+      Serial.print(F("0"));
+    }
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10)
+      Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
 
 void setup()
 {
-  // Serial.begin(115200);
+  Serial.begin(115200);
 
-  // Serial.println(F("setup()"));
-  //   display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  ss.begin(GPSBaud);
+
   display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
   // Serial.println(F("display.init() done"));
-  helloWorld();
-  // Serial.println(F("helloWorld() done"));
-  display.hibernate();
   // Serial.println(F("display.hibernate() done"));
 }
 
-void loop(){};
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+  {
+    gps.encode(ss.read());
+  }
+
+  if (millis() > 5000 && gps.time.isUpdated())
+  {
+    displayInfo();
+  }
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true)
+      ;
+  }
+}
