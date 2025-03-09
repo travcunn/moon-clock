8bdb75bcd5eabba039c6e6cc109d8874026fd50a
Travis Cunningham <traviscunn94@gmail.com>
2025-03-08 16:33:59 -0800

    Break out large functions into separate files

---
+++ src/timezone.h
@@ -0,0 +1,8 @@
+#ifndef DECODETIMEZONE_H
+#define DECODETIMEZONE_H
+
+// Returns a float representing the time zone offset (in hours)
+// based on the provided latitude (LAT) and longitude (LNG).
+float decodeTimezone(double LAT, double LNG);
+
+#endif // DECODETIMEZONE_H
