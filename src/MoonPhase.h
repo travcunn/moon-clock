a6814f9494cdd2eda526f1eff152276274899563
Travis Cunningham <traviscunn94@gmail.com>
2025-03-08 17:00:42 -0800

    Add documentation and comments to functions

--- src/MoonPhase.h
+++ src/MoonPhase.h
@@ -1,34 +1,69 @@
+/**
+ * @file moonphase.h
+ * @brief Header file for the moon phase calculation library
+ *
+ * Provides functionality to calculate various properties of the moon
+ * including phase, age, illumination fraction, distance, and position
+ * in the zodiac.
+ */
+
 #ifndef MoonPhase_h
 #define MoonPhase_h
 
 #include <time.h>
 
-#define MOON_SYNODIC_PERIOD 29.530588853 // Period of moon cycle in days.
-#define MOON_SYNODIC_OFFSET 2451550.26   // Reference cycle offset in days.
-#define MOON_DISTANCE_PERIOD 27.55454988 // Period of distance oscillation
-#define MOON_DISTANCE_OFFSET 2451562.2
-#define MOON_LATITUDE_PERIOD 27.212220817 // Latitude oscillation
-#define MOON_LATITUDE_OFFSET 2451565.2
-#define MOON_LONGITUDE_PERIOD 27.321582241 // Longitude oscillation
-#define MOON_LONGITUDE_OFFSET 2451555.8
+// Constants for moon calculations
+#define MOON_SYNODIC_PERIOD 29.530588853   // Period of moon cycle in days.
+#define MOON_SYNODIC_OFFSET 2451550.26     // Reference cycle offset in days.
+#define MOON_DISTANCE_PERIOD 27.55454988   // Period of distance oscillation
+#define MOON_DISTANCE_OFFSET 2451562.2     // Reference distance offset in days
+#define MOON_LATITUDE_PERIOD 27.212220817  // Latitude oscillation period in days
+#define MOON_LATITUDE_OFFSET 2451565.2     // Reference latitude offset in days
+#define MOON_LONGITUDE_PERIOD 27.321582241 // Longitude oscillation period in days
+#define MOON_LONGITUDE_OFFSET 2451555.8    // Reference longitude offset in days
 
+/**
+ * @class _MoonPhase
+ * @brief Class for calculating and storing moon phase and position information
+ *
+ * This class provides methods to calculate various properties of the moon
+ * based on a given time, including its phase, age, illumination fraction,
+ * distance from Earth, position in the sky, and zodiac constellation.
+ */
 class _MoonPhase
 {
 public:
-    double jDate;
-    double phase;           // 0 - 1, 0.5 = full
-    double age;             // Age in days of current cycle
-    double fraction;        // Fraction of illuminated disk
-    double distance;        // Moon distance in earth radii
-    double latitude;        // Moon ecliptic latitude
-    double longitude;       // Moon ecliptic longitude
-    const char *phaseName;  // New, Full, etc.
-    const char *zodiacName; // Constellation
+    double jDate;           ///< Julian date
+    double phase;           ///< Moon phase (0-1, 0=new, 0.5=full)
+    double age;             ///< Age in days of current lunar cycle
+    double fraction;        ///< Fraction of illuminated disk (0-1)
+    double distance;        ///< Moon distance in earth radii
+    double latitude;        ///< Moon ecliptic latitude in degrees
+    double longitude;       ///< Moon ecliptic longitude in degrees
+    const char *phaseName;  ///< Text name of phase (New, Full, etc.)
+    const char *zodiacName; ///< Zodiac constellation name
 
+    /**
+     * @brief Constructor for _MoonPhase class
+     *
+     * Initializes all member variables to default values.
+     */
     _MoonPhase();
-    void calculate(time_t);
+
+    /**
+     * @brief Calculate moon properties for the specified time
+     *
+     * @param t Unix timestamp (seconds since Jan 1, 1970)
+     */
+    void calculate(time_t t);
 
 private:
-    double _julianDate(time_t);
+    /**
+     * @brief Convert Unix time to Julian date
+     *
+     * @param t Unix timestamp (seconds since Jan 1, 1970)
+     * @return double Julian date
+     */
+    double _julianDate(time_t t);
 };
 #endif
--- src/timezone.h
+++ src/timezone.h
@@ -1,8 +1,24 @@
+/**
+ * @file timezone.h
+ * @brief Header file for timezone decoding functionality
+ *
+ * Provides functionality to determine the timezone offset based on
+ * geographical coordinates.
+ */
+
 #ifndef DECODETIMEZONE_H
 #define DECODETIMEZONE_H
 
-// Returns a float representing the time zone offset (in hours)
-// based on the provided latitude (LAT) and longitude (LNG).
+/**
+ * @brief Determines the timezone offset based on geographical coordinates
+ *
+ * This function uses the provided latitude and longitude to calculate
+ * the appropriate timezone offset in hours from UTC.
+ *
+ * @param LAT Latitude in decimal degrees (positive for North, negative for South)
+ * @param LNG Longitude in decimal degrees (positive for East, negative for West)
+ * @return float Timezone offset in hours from UTC
+ */
 float decodeTimezone(double LAT, double LNG);
 
 #endif // DECODETIMEZONE_H
